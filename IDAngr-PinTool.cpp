04bbc156699f8a61cb0ae4b1f9ae9f1bf8827837
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-12-10 15:24:34 +0100

    tab refactor

--- IDAngr-PinTool.cpp
+++ IDAngr-PinTool.cpp
@@ -5,27 +5,27 @@ static BOOL DebugInterpreter(THREADID, CONTEXT *, const string &, string *, VOID
 
 int main(int argc, char *argv[])
 {
-    PIN_InitSymbols();
-    if( PIN_Init(argc,argv) ) {
-        return 1;
-    }
+	PIN_InitSymbols();
+	if( PIN_Init(argc,argv) ) {
+		return 1;
+	}
 
-    PIN_AddDebugInterpreter(DebugInterpreter, 0);
+	PIN_AddDebugInterpreter(DebugInterpreter, 0);
 
-    PIN_StartProgram();
-    return 0;
+	PIN_StartProgram();
+	return 0;
 }
 
 
 static BOOL DebugInterpreter(THREADID tid, CONTEXT *ctxt, const string &cmd, string* result, VOID *)
 {
 	if (cmd == "idangr_getpid")
-    {
+	{
 		return GetStrPid(result);
-    }
+	}
 	if (cmd == "idangr_vmmap")
 	{
 		return VMMap(result);
 	}
-    return FALSE;
+	return FALSE;
 }
